9355d0dadeb026ccd23a49059bbb6e455dde6f28
Elson Gomes <elson.crg@gmail.com>
2025-11-30 13:30:31 -0100

    commit the initial part of my Casa - Kriola project login screen (Project - Final)

---
+++ README.md
@@ -0,0 +1,7 @@
+## CasaKriola
+
+No mundo de hoje, muitos estudantes sofrem pelo problema de encontrar uma casa para alugar, rendar ou ate mesmo dividir sem passa pelo transtorno de ficar em ospedado casa de alguns familiares ou amigos em algns casos.
+
+### O que ele resolve?
+
+- Ele basicamente da a oportunidade de voce escolher - do conforto da sua casa */ escolher a zona e o tipo de casa que deseja, e arrendar apa 
---
+++ src/Login.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button ,TextInput, TouchableOpacity} from 'react-native';
+import {useState} from 'react';
+import { Image } from 'react-native';
+
+export default function Login({navigation}){
+
+    const [box, setBox] = useState({
+        email: '',
+        password: 0
+    });
+    const [password, setPassword] = useState(0); 
+
+    return(
+        <View style ={styles.container}>
+            <View style={styles.header}>
+                <Image
+                    source={require("../assets/logo.png")}
+                    style={styles.logo}
+                />
+            </View>
+
+            <Text style={styles.title}>Segn in to Casa - Kriola</Text>
+
+            <Text style={styles.subtitle}>Get access to your home here</Text>
+
+        
+            <View style={styles.box}>
+                <View style={styles.input}>
+                    <Text style={styles.inputLabel}>Email address</Text>
+
+                    <TextInput
+                    keyboardType='email-address'
+                    placeholder='email@gmail.com' 
+                    value={box.email}
+                    onChangeText={email => setBox({...box, email})}
+                    style={styles.inputControl}
+                    />
+                </View>
+
+                <View style={styles.input}>
+                    <Text style={styles.inputLabel}>Password address</Text>
+
+                    <TextInput
+                    placeholder='*********' 
+                    value={box.password}
+                    onChangeText={password => setBox({...box, password})}
+                    style={styles.inputControl}
+                    />
+                </View>
+
+                <View>
+                    <TouchableOpacity
+                        onPress={()=> {}}
+                    />
+                    <View>
+                        <Text >Sign in</Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+   container: {
+    flex: 1,
+    backgroundColor: '#eee',
+    padding: 24
+   },
+   header:{
+    marginVertical: 36,
+   },
+   logo: {
+    width: 80,
+    height: 80,
+    alignSelf: 'center',
+    borderRadius: 10
+   },
+   /*}
+   box: {
+    backgroundColor: '#61B367',
+    padding: '40%',
+    borderRadius: 10
+   },*/
+   title:{
+    fontSize: 29,
+    fontWeight:'700',
+    marginBottom: 6,
+    textAlign: 'center'
+   },
+   subtitle:{
+    fontSize: 15,
+    fontWeight: '500',
+    color:'#929292',
+    textAlign: 'center',
+    marginBottom: 40
+   },
+   input:{
+    marginBottom: 30,
+   },
+   inputLabel:{
+    fontSize: 17,
+    fontWeight:'600',
+    color:'#222',
+    marginBottom: 16
+   },
+
+   inputControl:{
+    height: 44,
+    backgroundColor: '#ffff',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    fontSize: 15
+   },
+   btn:{
+    backgroundColor:'#7a5a5aff'
+   }
+});
